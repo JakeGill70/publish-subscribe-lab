3de3008adaaae38b775b5d27d766b0a857c19edd
Jake Gillenwater <JakeGill70@gmail.com>
2020-02-21 19:18:10 -0500

    Moved Script from HTML tag to file

--- static/js/subscriber.js
+++ static/js/subscriber.js
@@ -1,28 +1,121 @@
-$(document).ready(function(){
-    var socket = io.connect("http://" + document.domain + ":" + location.port + "/test");
-    var numbers_received = [];
+var socket = {}
+            var subscriber = {}
+            $(document).ready(function(){
+                var subscriberName = prompt("What is your subscriber name?");
 
-    socket.on("my response", function(msg){
-        $("#log").append("<p>Received: " + msg.data + "</p>");
-    });
+                socket = io.connect("http://" + document.domain + ":" + location.port + "/");
+                var notifications_received = [];
 
-    socket.on("newnumber", function(msg){
-        console.log(msg.number);
+                getSubscriberInfo(subscriberName);
 
-        if(numbers_received.length >= 10){
-            numbers_received.shift()
-        }
+                // Publish Subscriber Infomation
+                $("#subscriptionRequest").on("submit", function(e){
+                    // Prevent the form submission from refreshing the page
+                    e.preventDefault();
 
-        numbers_received.push(msg.number);
-        numbers_string = "";
-        for (let i = 0; i < numbers_received.length; i++) {
-            numbers_string = numbers_string + "<p>" + numbers_received[i].toString() + "</p>";
-        }
-        $("#log").html(numbers_string);
-    })
+                    // Get data from form
+                    var formDataRaw = $("#subscriptionRequest").serializeArray();
+                    // Format it into a single JS object
+                    var formData = convertFormArrayToAssociativeArray(formDataRaw);
+                    // Save a local copy for filtering
+                    subscriber = formData;
+                    // Send form data to the server as JSON
+                    socket.emit("subscriber", formData);
+                });
 
-    $("form#emit").submit(function(event){
-        socket.emit("my event", {data: $("#emit_data").val()});
-        return false;
-    });
-});
+                // Catch "publication" event
+                socket.on("publication", function(msg){
+                    // Convert the message content to a JSON string
+                    let content_string = JSON.stringify(msg.content);
+
+                    // Add the new notification if it updates the current subscriber information
+                    if(msg.content.subscriberName != undefined && msg.content.subscriberName == subscriber.subscriberName){
+                        notifications_received.push(content_string);
+                    }
+
+                    // If the notification is about a tell
+                    if(msg.content.id != undefined){
+                        // Add the new notification if it is about something that I have subscribed to
+                        let subTitles = subscriber.titles.split(",");
+                        let subTellers = subscriber.tellers.split(",");
+                        let subKeywords = subscriber.keywords.split(",");
+                        let conTitles = stringContainsAnyElement(msg.content.title, subTitles);
+                        let conTellers = stringContainsAnyElement(msg.content.teller, subTellers);
+                        let conKeywords = stringContainsAnyElement(msg.content.keyword, subKeywords);
+                        console.log([msg.content.title, subTitles, conTitles]);
+                        console.log([msg.content.teller, subTellers, conTellers]);
+                        console.log([msg.content.keyword, subKeywords, conTellers]);
+                        if(conTitles || conTellers || conKeywords){
+                                notifications_received.push(content_string);
+                        }
+                    }
+                    notifications_received.push("DEBUG: " + content_string);
+
+                    // Create the HTML for presenting all of the notifications
+                    numbers_string = "";
+                    for (let i = 0; i < notifications_received.length; i++) {
+                        numbers_string = numbers_string + "<p>" + notifications_received[i] + "</p>";
+                    }
+                    
+                    // Inject the HTML into the page
+                    $("#log").html(numbers_string);
+                })
+            });
+
+            function stringContainsAnyElement(mainString, strArray){
+                let output = false
+                strArray.forEach(subString => {
+                    subsString = $.trim(subString);
+                    if(subString !== "" && mainString.indexOf(subString > -1)){
+                        output = true;
+                    }
+                });
+                return output;
+            }
+
+            function getSubscriberInfo(subscriberName){
+                payload = {"subscriberName":subscriberName}
+                $.ajax({
+                    url:"./user",
+                    type:"GET",
+                    contentType:"application/json; charset=utf-8",
+                    data:payload,
+                    success:function(response){
+                        // If the user has an active subscription
+                        let subscriber = {}
+                        if(response != "none"){
+                            subscriber = JSON.parse(response);
+                        }
+                        else {
+                            subscriber = {"subscriberName": subscriberName,
+                                            "titles": "", "tellers": "", "keywords": ""}
+                        }
+                        console.log(["+++", subscriber, "+++"]);
+                        setSubscriberInfo(subscriber);
+                        populateFormWithSubscriberInfo(subscriber);
+                    }
+                })
+            }
+
+            function setSubscriberInfo(subscription){
+                subscriber = subscription;
+            }
+
+            function populateFormWithSubscriberInfo(subscriber){
+                let subscriberName = subscriber.subscriberName;
+                let titles = subscriber.titles;
+                let tellers = subscriber.tellers;
+                let keywords = subscriber.keywords;
+                $("#subscriberName").val(subscriberName);
+                $("#titles").val(titles);
+                $("#tellers").val(tellers);
+                $("#keywords").val(keywords);
+            }
+
+            function convertFormArrayToAssociativeArray(formArray){
+                var formDataFormatted = {};
+                formArray.forEach(input => {
+                    formDataFormatted[input.name] = input.value
+                });
+                return formDataFormatted
+            }
